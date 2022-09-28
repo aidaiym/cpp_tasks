3a8b355868bab2926f2a7861c61db87d4b5d3171
aidaiym <omurbekovnaaidaiym@gmail.com>
2022-09-28 15:12:50 +0600

    added two-dim arrays

--- lab5/tempCodeRunnerFile.cpp
+++ lab5/tempCodeRunnerFile.cpp
@@ -1,11 +1,10 @@
- // summ += arr[i][0];
-        // summ1  += arr[i][1];
-        // summ2  += arr[i][2];
-        // summ3  += arr[i][3];
-        // summ4  += arr[i][4];
-        // summ5  += arr[i][5];
-        // arithmetic_mean = summ/3;
-        // arithmetic_mean = summ/3;
-        // arithmetic_mean = summ/3;
-        // arithmetic_mean = summ/3;
-        // arithmetic_mean = summ/3;
+  // cout << "Entered 2D Array: " << endl;
+
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     for (int j = 0; j < 8; j++)
+    //     {
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }

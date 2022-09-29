881fe2304e57d813ea353d9795759a299f5df65c
aidaiym <omurbekovnaaidaiym@gmail.com>
2022-09-29 16:42:19 +0600

    done multdimansional_array

--- lab5/tempCodeRunnerFile.cpp
+++ lab5/tempCodeRunnerFile.cpp
@@ -1,10 +1,2 @@
-  // cout << "Entered 2D Array: " << endl;
-
-    // for (int i = 0; i < 8; i++)
-    // {
-    //     for (int j = 0; j < 8; j++)
-    //     {
-    //         cout << arr[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
+   x = arr[k][i];
+                    arr[k][i] = arr[k][j];
